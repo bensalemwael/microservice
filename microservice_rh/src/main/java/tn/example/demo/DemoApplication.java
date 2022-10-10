5be411722f1d174cf76f247a4a2ff3f9e9aed71e
= <=>
2022-10-10 16:35:09 +0100

    update structure

--- microservice_rh/src/main/java/tn/example/demo/DemoApplication.java
+++ microservice_rh/src/main/java/tn/example/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package tn.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
