833f4e8bc4e7e09761784ed599aa3e87f11960e6
bensalemwael <waelbensalem40@gmail.com>
2022-10-17 19:04:57 +0100

    add product

---
+++ Dashboard/src/app/models/Product.ts
@@ -0,0 +1,11 @@
+import { Category } from "./Categories";
+
+export class Product {
+    productId: number;
+    name: string;
+    quantity: number;
+    size: number;
+    price: number;
+    category: Category;
+    
+}
