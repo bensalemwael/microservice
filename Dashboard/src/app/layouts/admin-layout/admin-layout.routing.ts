2208fed7c02c5a8c166c700f32a7d78b7878e484
bensalemwael <waelbensalem40@gmail.com>
2022-10-19 21:23:11 +0100

    update crud category

--- Dashboard/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ Dashboard/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -6,12 +6,12 @@ import { MapsComponent } from '../../pages/maps/maps.component';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { TablesComponent } from '../../pages/tables/tables.component';
 import { UsersComponent } from 'src/app/pages/users/users.component';
-import { ProductsComponent } from 'src/app/pages/products/list-products/products.component';
 import { AddProductComponent } from 'src/app/pages/products/add-product/add-product.component';
 import { EditProductComponent } from 'src/app/pages/products/edit-product/edit-product.component';
 import { ListCategoriesComponent } from 'src/app/pages/categories/list-categories/list-categories.component';
 import { EditCategoriesComponent } from 'src/app/pages/categories/edit-categories/edit-categories.component';
 import { AddCategoriesComponent } from 'src/app/pages/categories/add-categories/add-categories.component';
+import { ProductsComponent } from 'src/app/pages/products/list-products/products.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
