490f746b3b34d5c85cc2ee8f2f19a23f3935c078
= <=>
2022-10-20 18:04:38 +0100

    update

--- Dashboard/src/app/app.module.ts
+++ Dashboard/src/app/app.module.ts
@@ -27,6 +27,12 @@ import { EditUserComponent } from './pages/users/edit-user/edit-user.component';
 import { AddAbsenceComponent } from './pages/absences/add-absence/add-absence.component';
 import { ListProductComponent } from './pages/products/list-product/list-product.component';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { EventsComponent } from './pages/events/events.component';
+import { AddEventComponent } from './pages/events/add-event/add-event.component';
+import { EditEventComponent } from './pages/events/edit-event/edit-event.component';
+import { PartnersComponent } from './pages/partners/partners.component';
+import { AddPartnerComponent } from './pages/partners/add-partner/add-partner.component';
+import { EditPartnerComponent } from './pages/partners/edit-partner/edit-partner.component';
 
 
 @NgModule({
@@ -59,7 +65,13 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
     AddUserComponent,
     EditUserComponent,
     AddAbsenceComponent,
-    ListProductComponent
+    ListProductComponent,
+    EventsComponent,
+    AddEventComponent,
+    EditEventComponent,
+    PartnersComponent,
+    AddPartnerComponent,
+    EditPartnerComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
